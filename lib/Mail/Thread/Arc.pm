110a92052283ede4a218ac60eaa28186a8b8ce0c
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-12-10 12:27:22 +0000

    state we use imager.  intended api docs
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Mail-Thread-Arc@1834 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Mail/Thread/Arc.pm
+++ lib/Mail/Thread/Arc.pm
@@ -3,7 +3,37 @@ package Mail::Thread::Arc;
 use Imager;
 our $VERSION = '1.00';
 
+=head1 NAME
+
+Mail::Thread::Arc - Generates a Thread Arc reperesentation of a thread
+
+=head1 SYNOPSIS
+
+ my $threader = Mail::Thread->new( @messages );
+ my $arc = Mail::Thread::Arc->new;
+
+ $threader->thread;
+
+ my $i;
+ for my $thread ($threader->rootset) {
+     ++$i;
+     my $imager = $arc->render( $thread );
+     $imager->write( file => "thread_$i.png" )
+       or die $imager->errstr;
+ }
+
+=head1 DESCRIPTION
+
+Mail::Thread::Arc takes a Mail::Thread::Container and generates an
+image of the Thread Arc.  Thread Arcs are described in the
+documentation for IBM's remail project.
+
+http://www.research.ibm.com/remail/
+
+=cut
+
 sub new {
 }
 
 1;
+__END__
