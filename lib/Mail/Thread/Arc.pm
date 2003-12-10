73261c41802f0ea3d03bb3bb8706e08254cccb9f
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-12-10 14:38:49 +0000

    makes spots
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Mail-Thread-Arc@1838 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Mail/Thread/Arc.pm
+++ lib/Mail/Thread/Arc.pm
@@ -1,9 +1,11 @@
 use strict;
 package Mail::Thread::Arc;
-our $VERSION = '1.00';
-use base qw( Class::Accessor::Chained::Fast );
-__PACKAGE__->mk_accessors( 'imager' );
 use Imager;
+use Date::Parse qw( str2time );
+use base qw( Class::Accessor::Chained::Fast );
+__PACKAGE__->mk_accessors(qw( messages imager ));
+
+our $VERSION = '1.00';
 
 =head1 NAME
 
@@ -53,15 +55,37 @@ Renders the thread tree as an image arc.  Returns an Imager object.
 
 sub render {
     my $self = shift;
+    my $root = shift;
 
-    $self->imager( Imager->new );
-
+    # extract just the containers with messages
     my @messages;
-    # sort all the messages chronologically
-    # find the generation depth of each message
+    $root->iterate_down(
+        sub {
+            my $container = shift;
+            push @messages, $container if $container->message;
+        } );
 
+    # sort on date
+    @messages = sort {
+        $self->date_of( $a ) <=> $self->date_of( $b )
+    } @messages;
+
+    my $imager = Imager->new(
+        xsize => ( @messages + 1 ) * $self->message_radius * 2,
+        ysize => $self->message_radius * 2 + $self->max_arc_radius * 2,
+       )
+      or die Imager->errstr;
+
+    $self->imager( $imager );
+
+    $self->imager->flood_fill( x => 1, y => 1, color => '#ffffff' );
+
+
+    $self->messages( {} );
+    my $i;
     for my $message (@messages) {
         # place the message
+        $self->messages->{$message} = ++$i;
         $self->draw_message( $message );
         next unless $message->parent;
         $self->draw_arc( $message->parent, $message );
@@ -70,19 +94,49 @@ sub render {
     return $self->imager;
 }
 
+sub message_radius {
+    20;
+}
+
+sub max_arc_radius {
+    20;
+}
+
+sub date_of {
+    my ($self, $container) = @_;
+    return str2time( Email::Thread->_get_hdr( $container->message, 'date' ) );
+}
+
 sub draw_message {
     my ($self, $message) = @_;
+    $self->imager->circle( color => '#000000',
+                           r => $self->message_radius / 2,
+                           x => $self->messages->{$message} * $self->message_radius * 2,
+                           y => $self->max_arc_radius +
+                                $self->message_radius);
 }
 
 sub draw_arc {
     my ($self, $from, $to) = @_;
 
-    if ($to->generation % 2) {
+    if ($self->thread_generation( $to ) % 2) {
         # draw arc above
     }
     else {
         # draw arc below
     }
+}
+
+sub thread_generation {
+    my ($self, $container) = @_;
+
+    my $count = 0;
+    while ($container->parent) {
+        ++$count;
+        $container = $container->parent;
+    }
+
+    return $count;
 }
 
 1;
